3a61c76dbcaee2d11857c95472d321e7e1fd4297
Colin Budka <colin.budka@gmail.com>
2018-06-27 20:09:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,9 +3,17 @@ function getIt(){
   $("p").on("click", alert("Hey!"))
 }
 function frameIt(){
-  $("img").on("load", ("tasty"))
+ // $("img").on("load", ("tasty"))
 }
-
+function pressIt(){
+  $("form"),on("keydown", 
+    function(key){
+      if(key.which == 82){
+        alert("r was pressed")
+      }
+    }
+  )
+}
 
 $(document).ready(function(){
 
